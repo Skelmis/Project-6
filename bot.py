aca6c562ecf31e4cbb4bba5650777e2964209454
Skelmis <skelmis.craft@gmail.com>
2021-10-04 22:46:17 +1300

    Add image comparision

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
+import asyncio
 import logging
 import os
 from pprint import pprint
 
+import cv2
 import discord
 from discord.ext import commands
 
@@ -36,11 +38,85 @@ async def pic(ctx) -> None:
     await ctx.send("Imagine taking pictures of people. Tut.", file=file)
 
 
+@bot.command(aliases=["comparepicture"])
+async def com(ctx):
+    image = bot.cv.take_picture()
+    path_one = bot.cv.save_picture(image)
+    file = discord.File(path_one)
+    # await ctx.send(file=file)
+    print("Taken first")
+
+    await asyncio.sleep(10)
+    image_two = bot.cv.take_picture()
+    path_two = bot.cv.save_picture(image_two)
+    print("Taken second")
+
+    file = discord.File(path_two)
+    # await ctx.send(file=file)
+
+    diff_score, actual_diffs = bot.cv.compare_images(image, image_two)
+    thresh, cnts = bot.cv.visualize_image_differences(actual_diffs)
+
+    # loop over the contours
+    for c in cnts:
+        pprint(c)
+        # compute the bounding box of the contour and then draw the
+        # bounding box on both input images to represent where the two
+        # images differ
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        cv2.rectangle(image_two, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+        return
+
+    await ctx.send("Here is the output for differences")
+    # show the output images
+    path = bot.cv.save_picture(image)
+    file = discord.File(path)
+    await ctx.send(file=file)
+
+    path = bot.cv.save_picture(image_two)
+    file = discord.File(path)
+    await ctx.send(file=file)
+
+    path = bot.cv.save_picture(actual_diffs)
+    file = discord.File(path)
+    await ctx.send(file=file)
+
+    path = bot.cv.save_picture(thresh)
+    file = discord.File(path)
+    await ctx.send(file=file)
+
+    await ctx.send(":thumbsup:")
+
+
 @bot.command()
+async def diff(ctx):
+    """Given two images, return a box around the differences"""
+    image_one = bot.cv.take_picture()
+    path_one = bot.cv.save_picture(image_one)
+    await asyncio.sleep(5)
+
+    image_two = bot.cv.take_picture()
+    path_two = bot.cv.save_picture(image_two)
+
+    diff_score, actual_diffs = bot.cv.compare_images(image_one, image_two)
+    if diff_score > float(0.9):
+        await ctx.send(
+            f"These images appear too similar to figure out, will try regardless.\nSSIM: {diff_score}"
+        )
+
+    _, contours = bot.cv.visualize_image_differences(actual_diffs)
+    print(type(contours), len(contours))
+
+    print(diff_score)
+
+
+@bot.command(aliases=["l"])
 @commands.is_owner()
 async def logout(ctx) -> None:
     """Gracefully shuts the bot down"""
-    bot.cv.cleanup()
+    bot.cv.release()
     await ctx.send("Logging out")
     await bot.close()
 
--- cv/cv.py
+++ cv/cv.py
@@ -1,8 +1,11 @@
+import imutils as imutils
+import numpy as np
 import itertools
 import logging
 import os
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
+from skimage.metrics import structural_similarity as compare_ssim
 
 import cv2
 
@@ -11,23 +14,43 @@ from bot_base import BotBase
 log = logging.getLogger(__name__)
 
 
+def inject_cam(f):
+    def wrapper(*args):
+        """Reset video capture when not in use"""
+        self = args[0]
+        self.feed = cv2.VideoCapture(0)
+
+        values = f(*args)
+
+        self.release()
+        self.feed = None
+
+        return values
+
+    return wrapper
+
+
 class CV:
     def __init__(self, bot):
         self.bot: BotBase = bot
-        self.feed = cv2.VideoCapture(0)
+        self.feed = None
         self.cwd = str(Path(__file__).parents[0])
 
         self.image_suffix = itertools.count().__next__
 
+        self.current_frame = None
+
+    @inject_cam
     def take_picture(self):
         """Takes a picture and saves it
+
+        This runs as a background thread to keep this shit running, idk
 
         Returns
         -------
         The grabbed image
         """
         ret, frame = self.feed.read()
-        self.feed.release()
         if not ret:
             log.warning("No video feed active to capture from.")
             return None
@@ -55,9 +78,53 @@ class CV:
 
         return path
 
-    def cleanup(self) -> None:
+    def compare_images(
+        self, image_one, image_two
+    ) -> (Union[float, np.ndarray], np.ndarray):
+        """
+        Compares two images and returns the MSSIM or
+        mean structural similarity over the image
+
+        Returns
+        -------
+        Union[float, np.ndarray]
+            The mean structural similarity
+        np.ndarray
+            The full SSIM image for visual purposes
+
+        Notes
+        -----
+        Comparison is done at a
+        grey scale level
+        """
+        image_one_grey = cv2.cvtColor(image_one, cv2.COLOR_BGR2GRAY)
+        image_two_grey = cv2.cvtColor(image_two, cv2.COLOR_BGR2GRAY)
+
+        score, diff = compare_ssim(image_one_grey, image_two_grey, full=True)
+        diff: np.ndarray = (diff * 255).astype("uint8")
+
+        # noinspection PyTypeChecker
+        return score, diff
+
+    def visualize_image_differences(self, diff: np.ndarray):
+        """
+        Visualize the differences between images
+        """
+        thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        cnts = cv2.findContours(
+            thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+        )
+        cnts = imutils.grab_contours(cnts)
+
+        return thresh, cnts
+
+    def get_biggest_difference(self, contours):
+        pass
+
+    def release(self) -> None:
         """Cleans up video objects before shutdown"""
-        self.feed.release()
+        if self.feed:
+            self.feed.release()
 
         # Destroy all the windows
         cv2.destroyAllWindows()
