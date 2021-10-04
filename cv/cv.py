c85d227acf287be4b50b4f02b00b59c9227a8901
Skelmis <skelmis.craft@gmail.com>
2021-10-05 00:04:40 +1300

    Add dynamic box sizing

--- cv/cv.py
+++ cv/cv.py
@@ -188,7 +188,7 @@ class CV:
             else:
                 color = color_enum.get_bgr()
             # How long to draw points
-            size = 4
+            size = ((x + y) // 2) // 50
             x_line_length = int(x / 25)
             y_line_length = int(y / 25)
 
