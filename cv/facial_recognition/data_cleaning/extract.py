df09a1cff139df2ffed975b450101046195a28df
Skelmis <skelmis.craft@gmail.com>
2021-10-09 02:57:07 +1300

    Add image extraction

--- cv/facial_recognition/data_cleaning/extract.py
+++ cv/facial_recognition/data_cleaning/extract.py
@@ -1,40 +1,78 @@
-def find_face(self, image: np.ndarray):
+import os
+from pathlib import Path
+
+import cv2
+import numpy as np
+
+upper_cwd = str(Path(__file__).parents[1])
+cwd = str(Path(__file__).parents[0])
+
+min_face_confidence = 0.5
+counter = 0
+
+
+def image_to_blob(image: np.ndarray):
+    """
+    Given an image, return its blob equivalent
+    """
+    return cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)))
+
+
+net: cv2.dnn_Net = cv2.dnn.readNetFromCaffe(
+    os.path.join(upper_cwd, "caffe", "deploy.prototxt"),
+    os.path.join(upper_cwd, "caffe", "opencv_face_detector.caffemodel"),
+)
+
+
+def find_face(image: np.ndarray, file_type: str):
     """Entry to this class for predictions"""
-    h, w = image.shape[:2]
+    try:
+        h, w = image.shape[:2]
+    except:
+        return False
 
-    net: cv2.dnn_Net = cv2.dnn.readNetFromCaffe(
-        os.path.join(self.cwd, "caffe", "deploy.prototxt"),
-        os.path.join(self.cwd, "caffe", "opencv_face_detector.caffemodel"),
-    )
-
-    blob = self.image_to_blob(image)
+    blob = image_to_blob(image)
     net.setInput(blob)
 
     detections = net.forward()
-
     for i in range(0, detections.shape[2]):
         # extract the confidence (i.e., probability) associated with the
         # prediction
         confidence = detections[0, 0, i, 2]
         # filter out weak detections by ensuring the `confidence` is
         # greater than the minimum confidence
-        if confidence > self.min_face_confidence:
+        if confidence > min_face_confidence:
             # compute the (x, y)-coordinates of the bounding box for the
             # object
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
 
-            # draw the bounding box of the face along with the associated
-            # probability
-            text = "{:.2f}%".format(confidence * 100)
-            y = startY - 10 if startY - 10 > 10 else startY + 10
-            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
-            cv2.putText(
-                image,
-                text,
-                (startX, y),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.45,
-                (0, 0, 255),
-                2,
-            )
+            try:
+                cropped_image = image[
+                    (startY - 100) : (endY + 100), (startX - 100) : (endX + 100)
+                ]
+                global counter
+                cv2.imwrite(
+                    os.path.join(cwd, "output", f"{counter}.{file_type}"), cropped_image
+                )
+                counter += 1
+            except:
+                continue
+
+
+def main():
+    with os.scandir(os.path.join(cwd, "input")) as scanner:
+        for photo_path in scanner:
+            path: str = photo_path.path
+            print(path)
+            if path.endswith("png"):
+                file_type = "png"
+            else:
+                file_type = "jpeg"
+
+            photo = cv2.imread(path)
+            find_face(photo, file_type)
+
+
+if __name__ == "__main__":
+    main()
